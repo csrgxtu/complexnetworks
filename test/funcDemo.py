eef8053fd3f72effc03ebdbf84e3aa28687a7d1f
archer <1246506786@qq.com>
2015-10-26 22:14:30 -0400

    funcDemo

---
+++ test/funcDemo.py
@@ -0,0 +1,15 @@
+#!/usr/local/env python
+#
+# Author: Archer Reilly
+# Date: 26/Aug/2015
+# File: funcDemo.py
+# Desc: illustrate how to use funcs in Python
+#
+# Produced By BR
+
+def readFile(filename):
+    print 'Starting read ' + filename + ' into mem'
+
+if __name__ == '__main__':
+    readFile('hello.txt')
+
